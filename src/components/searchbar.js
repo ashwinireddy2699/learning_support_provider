a8638233892bbe6899f1aac6caaca499689eda13
Ashwini Reddy <ashwinireddy2699@gmail.com>
2025-06-15 20:21:59 +0530

    added search bar logic

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import styles from '../styles/serachbar.module.css'
 
-function Searchbar() {
+function Searchbar({search,setSearchTerm}) {
     return (
         <div className={styles.SearchContainer}>
+             <input 
+             type="text"
+             value={search}
+             onChange={(e)=>setSearchTerm(e.target.value)}
+             className={styles.searchInput}
+             placeholder='Search by name or specialization'
+             />
             
-
+     
 
         </div>
     )
